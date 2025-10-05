663836690c47dd3f34da3b5dbd0e4426fc5cb80e
Alex Matcov <alex.matcov@gmail.com>
2025-10-05 19:16:59 +0200

    add check for checkov installation

---
+++ tutorial/step2/verify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if command -v checkov &> /dev/null; then
+    echo "Checkov is installed"
+else
+    echo "Checkov is not installed or not in PATH"
+    echo "Please wait for the installation to complete"
+    exit 1
+fi
