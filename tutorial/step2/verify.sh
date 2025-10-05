e6bee550f24e5e6f77f0722d328ab0b4052432d8
Alex Matcov <alex.matcov@gmail.com>
2025-10-05 22:23:49 +0200

    fix: delete prints

--- tutorial/step2/verify.sh
+++ tutorial/step2/verify.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 if command -v checkov >/dev/null 2>&1 && checkov -v >/dev/null 2>&1; then
-    echo "Checkov is installed"
     exit 0
 else
-    echo "Checkov is not installed or not in PATH"
-    echo "Please wait for the installation to complete"
     exit 1
 fi
