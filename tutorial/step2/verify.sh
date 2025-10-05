dfb7d73752c35a7c2faad395157a7cbd4cc17264
Alex Matcov <alex.matcov@gmail.com>
2025-10-05 22:13:18 +0200

    fix: add exit point for first condition

--- tutorial/step2/verify.sh
+++ tutorial/step2/verify.sh
@@ -2,6 +2,7 @@
 
 if command -v checkov &> /dev/null; then
     echo "Checkov is installed"
+    exit 0
 else
     echo "Checkov is not installed or not in PATH"
     echo "Please wait for the installation to complete"
